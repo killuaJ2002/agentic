88d4b31fcbd74129b853f17b226fcf1b7e4f4a97
killuaJ2002 <kjcodes2002@gmail.com>
2025-07-31 23:50:25 +0530

    updated webIntereactor to fill From input

--- webInteractor.js
+++ webInteractor.js
@@ -1,26 +1,44 @@
 import { chromium } from "playwright";
 
 const bookTicket = async () => {
-  const browser = await chromium.launch({ headless: false });
-  const page = await browser.newPage();
+  const browser = await chromium.launch({ headless: false, slowMo: 1000 });
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
   try {
-    await page.goto("https://www.makemytrip.com/");
-    const closeBtn = page.locator(".commonModal__close");
-    await closeBtn.waitFor({ state: "visible", timeout: 3000 });
-    await closeBtn.click();
-    await page.locator(".searchCity").click();
-    const fromInput = page.locator("input[placeholder='From']");
-    await fromInput.fill("Dehradun");
+    await page.goto("https://www.makemytrip.com/", {
+      waitUntil: "domcontentloaded",
+    });
 
-    await page.waitForSelector("ul[role='listbox'] li[role='option']"); // Wait for suggestions
-    await page.locator("ul[role='listbox'] li[role='option']").first().click();
+    // close popups if appear
+    const closeBtn = page.locator(".commonModal__close");
+    if (await closeBtn.isVisible({ timeout: 5000 })) {
+      await closeBtn.click();
+      console.log("Closed popup");
+    } else {
+      console.log("No popup appeared");
+    }
+
+    // Selecting flight tab
+    await page.locator('li[data-cy="menu_Flights"]').click();
+    // Selecting one way trip
+    await page.locator('li[data-cy="oneWayTrip"]').click();
+    // Selecting From city
+    await page.locator('label[for="fromCity"]').click();
+    await page.locator('input[placeholder="From"]').fill("Silchar");
+    // waiting for suggestion list to appear
+    await page.waitForSelector(".react-autosuggest__suggestions-list");
+    // select the first li
+    await page
+      .locator(".react-autosuggest__suggestions-list li")
+      .first()
+      .click();
   } catch (error) {
     console.log(error);
   } finally {
-    setTimeout(async () => {
-      await browser.close();
-    }, 3000);
+    await page.waitForTimeout(3000); // Wait 3 sec visibly
+    await browser.close();
   }
 };
 
